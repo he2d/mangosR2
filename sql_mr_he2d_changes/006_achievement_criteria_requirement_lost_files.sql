0e15f3a452bcc33595518a3ef227fab167407ffc
He2d <admin@craenkland.de>
2011-09-11 16:01:47 +0200

    lost db achievement from script

---
+++ sql_mr_he2d_changes/006_achievement_criteria_requirement_lost_files.sql
@@ -0,0 +1,30 @@
+-- ----------------------------
+-- Records of achievement_criteria_requirement
+-- ----------------------------
+INSERT INTO `achievement_criteria_requirement` VALUES ('5726', '0', '0', '0');
+INSERT INTO `achievement_criteria_requirement` VALUES ('5727', '0', '0', '0');
+INSERT INTO `achievement_criteria_requirement` VALUES ('5728', '0', '0', '0');
+INSERT INTO `achievement_criteria_requirement` VALUES ('5734', '0', '0', '0');
+INSERT INTO `achievement_criteria_requirement` VALUES ('5735', '0', '0', '0');
+INSERT INTO `achievement_criteria_requirement` VALUES ('5736', '0', '0', '0');
+INSERT INTO `achievement_criteria_requirement` VALUES ('5737', '0', '0', '0');
+INSERT INTO `achievement_criteria_requirement` VALUES ('5738', '0', '0', '0');
+INSERT INTO `achievement_criteria_requirement` VALUES ('5739', '0', '0', '0');
+INSERT INTO `achievement_criteria_requirement` VALUES ('5809', '0', '0', '0');
+INSERT INTO `achievement_criteria_requirement` VALUES ('5810', '0', '0', '0');
+INSERT INTO `achievement_criteria_requirement` VALUES ('8587', '0', '0', '0');
+INSERT INTO `achievement_criteria_requirement` VALUES ('8588', '0', '0', '0');
+INSERT INTO `achievement_criteria_requirement` VALUES ('8589', '0', '0', '0');
+INSERT INTO `achievement_criteria_requirement` VALUES ('8590', '0', '0', '0');
+INSERT INTO `achievement_criteria_requirement` VALUES ('8599', '0', '0', '0');
+INSERT INTO `achievement_criteria_requirement` VALUES ('8600', '0', '0', '0');
+INSERT INTO `achievement_criteria_requirement` VALUES ('8601', '0', '0', '0');
+INSERT INTO `achievement_criteria_requirement` VALUES ('8602', '0', '0', '0');
+INSERT INTO `achievement_criteria_requirement` VALUES ('8607', '0', '0', '0');
+INSERT INTO `achievement_criteria_requirement` VALUES ('8608', '0', '0', '0');
+INSERT INTO `achievement_criteria_requirement` VALUES ('8609', '0', '0', '0');
+INSERT INTO `achievement_criteria_requirement` VALUES ('8610', '0', '0', '0');
+INSERT INTO `achievement_criteria_requirement` VALUES ('8615', '0', '0', '0');
+INSERT INTO `achievement_criteria_requirement` VALUES ('8616', '0', '0', '0');
+INSERT INTO `achievement_criteria_requirement` VALUES ('8617', '0', '0', '0');
+INSERT INTO `achievement_criteria_requirement` VALUES ('8618', '0', '0', '0');
